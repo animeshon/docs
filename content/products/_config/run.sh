3ac3ee17c00009b79b017c413794db870b2afa92
Christian Roggia <christian.roggia@gmail.com>
2022-07-11 01:30:45 +0200

    feat: add configuration for all products

--- content/products/_config/run.sh
+++ content/products/_config/run.sh
@@ -1,7 +1,39 @@
-gotemplate run \
-  --import ./_config/image.json \
-  --source ./_template \
-  --target ./image/documentation/docs \
-  --base \
-  --overwrite \
-  $(cd _template && find . -type f)
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+declare -a products=(
+  "bridge"
+  "credentials"
+  "crossrefs"
+  "iam"
+  "identity"
+  "image"
+  "knowledge"
+  "library"
+  "multimedia"
+  "payment"
+  "product"
+  "release"
+  "resourcemanager"
+  "tracker"
+  "vision"
+  "webcache"
+  "webpage"
+  "websearch"
+)
+
+for i in "${products[@]}"
+do
+  gotemplate run \
+    --import ./_config/${i}.json \
+    --source ./_template \
+    --target ./${i}/documentation/docs \
+    --base \
+    --overwrite \
+    $(cd _template && find . -type f)
+done
+
+exit 0
