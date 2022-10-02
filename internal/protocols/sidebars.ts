993f15a59b85ce6a6fcaa9bf5c8011019f5929a0
Luscha <matteo.roggia@gmail.com>
2022-10-02 19:07:33 +0200

    feat: Graphql quickstarts folder and example quickstart

--- internal/protocols/sidebars.ts
+++ internal/protocols/sidebars.ts
@@ -1,7 +1,23 @@
 module.exports = {
   protocols: [
     'welcome',
-    'graphql',
+    
+    {
+      type: 'category',
+      label: 'GraphQL',
+      collapsed: true,
+      items: [
+        'graphql/index',
+        {
+          type: 'category',
+          label: 'Quickstarts',
+          collapsed: true,
+          items: [
+            "graphql/quickstarts/oracolo-temporal-search",
+          ],
+        },
+      ],
+    },
     'grpc',
     'rest',
   ],
